f68d7ea928b19c058b55c819074ff0fab3a395fd
Teja8352301 <yerraguntlateja4@gmail.com>
2022-06-11 10:43:16 +0530

    fix

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ const dbConnect = async(isProduction) =>{
     if(isProduction == 'true'){
         uri = 'mongodb+srv://Teja:w1J9teVj2hcaIsM9@cluster0.zl9y0.mongodb.net/?retryWrites=true&w=majority'
     }
+    console.log('url need to connect...',uri)
     const client = new MongoClient(uri,{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     // client.db('users')
     await client.connect();
